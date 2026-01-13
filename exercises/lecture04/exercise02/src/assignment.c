b396db45d0b64a15bcc17033f2c16e845e1776a9
= <erik>
2026-01-13 17:04:10 +0100

    Forgot some things.

--- exercises/lecture04/exercise02/src/assignment.c
+++ exercises/lecture04/exercise02/src/assignment.c
@@ -1,20 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+// In this C exercise, you will test some very basic
+// C programming by implementing a small part of a 
+// simple RPG combat system. 
+// 
+// Start simserver with a console connected as usual, 
+// and try to pass all tests. 
+//
+// Do the assignments in order, as each assignment
+// builds on the previous one.
+///////////////////////////////////////////////////////////////////////////////
+
 #include <stdint.h>
 #define SOLUTION
 
 int hero_strength = 0; 
 int hero_dexterity = 0;
 int hero_weapon = 0; // 0 = unarmed, 1 = sword, 2 = axe, 3 = spear
-int enemy_armour = 0; // Array of armour values
+int enemy_armour = 0; 
 
 int enemy_defense()
 {
-    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
     // Assignment 3: 
     // ====================================================
     // Enemy defense is enemy_armour * 2
     // Every time the hero attacks, the enemy's armour
     // is reduced by 1 (to a minimum of 0)
-    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 #ifdef SOLUTION    
     int defense = enemy_armour * 2;
     if (enemy_armour > 0) {
@@ -28,7 +40,7 @@ int enemy_defense()
 
 int weapon_bonus()
 {
-    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
     // Assignment 2: 
     // ====================================================
     // Return weapon bonus based on hero_weapon value.
@@ -37,7 +49,7 @@ int weapon_bonus()
     // sword = 5
     // axe = 7
     // spear = 6
-    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 #ifdef SOLUTION    
     switch (hero_weapon)
     {
@@ -52,14 +64,14 @@ int weapon_bonus()
 
 int hero_damage()
 {
-    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
     // Assignment 1: 
     // ====================================================
     // Calculate damage dealt in unarmed combat against
     // a monster without armour:
     // DAMAGE = STRENGTH * 2 + DEXTERITY + 
     //              (weapon_bonus() - enemy_armour())
-    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 #ifdef SOLUTION    
     int damage = hero_strength * 2 + hero_dexterity + (weapon_bonus() - enemy_defense());
     return damage;

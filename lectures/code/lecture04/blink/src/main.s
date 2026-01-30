6ae161175b196911ed839b73291d05a777c30ae7
= <erik>
2026-01-30 11:56:29 +0100

    Lecture04 done

--- lectures/code/lecture04/blink/src/main.s
+++ lectures/code/lecture04/blink/src/main.s
@@ -1,33 +1,77 @@
 .section .text
 .globl main
 
+###############################################################################
+# 1: Gå igenom dessa addressuträkningar
+###############################################################################
 .equ GPIO_D_CFGLR, 0x40011400
 .equ GPIO_D_CFGHR, 0x40011404
 .equ GPIO_D_INDR,  0x40011408
 .equ GPIO_D_OUTDR, 0x4001140C
 
 main:
+    ###############################################################################
+    # 2: Gå långsamt igenom hur vi konfigurerar registret
+    ###############################################################################
+
     # Configure GPIO D, pin 2 as output, 2Mhz, push-pull 
     la t0, GPIO_D_CFGLR
     lw t1, 0(t0)
-    andi t1, t1, 0xFFFFF0FF  # Clear bits for pin 2
-    ori t1, t1, 0x00000200   # Set pin 2 
+    li t2, 0xFFFFF0FF
+    and t1, t1, t2
+    li t2, 0x00000200  # Clear bits for pin 2
+    or t1, t1, t2   # Set pin 2 
     sw t1, 0(t0)
 
-
-
-    la t0, GPIO_D_CFGHR
-    li t1, 0x88888888
-    sw t1, 0(t0)  # Set upper 8 pins as input with pull-up/down
 loop:
-    la t0, GPIO_D_INDR
-    lw t1, 0(t0)          # Read input from DIP switch
-    srl t1, t1, 8        # Shift right to get upper
-    andi t1, t1, 0xFF     # Mask to get only upper 8 bits
-    la t0, GPIO_D_OUTDR
-    sw t1, 0(t0)          # Write to bargraph
+    ###############################################################################
+    # 3: Blinka. 
+    #    1. Först utan s0, och utan delay
+    #    2. Visa att vi kan stega igenom men om vi kör lyser den svagt
+    #    3. Lägg till delay funktionen
+    #       Om tid finnes: Resonera kring hur lång delay vi behöver
+    #    4. Fråga vad som gick fel
+    #    5. Använd s0
+    ###############################################################################
+
+    la s0, GPIO_D_OUTDR
+    li t1, 0b100
+    sw t1, 0(s0)          
+
+    call delay
+
+    li t1, 0b000
+    sw t1, 0(s0)          
+
+    call delay
+
     j loop                # Repeat indefinitely
     ret
 
+
+delay: 
+    li t0, 0xFFFFFF
+delay_loop: 
+    addi t0, t0, -1
+    bnez t0, delay_loop
+    ret
+
+
+
+.equ GPIO_D_BSHR, 0x40011410
+.equ GPIO_D_BCR,  0x40011414
+
+main: 
+    # Konfigurera pinnen
+    ...
+    # Skriv 1 till pin 2
+    la t0, GPIO_D_BSHR
+    li t1, 0b100
+    sh t1, 0(t0)
+    
+    # Eller skriv 0    
+    la t0, GPIO_D_BCR
+    li t1, 0b100
+    sh t1, 0(t0)
 
 
